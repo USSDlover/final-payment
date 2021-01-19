02e4667026dae488692f2e55bbdd645d9952f178
Alireza Razinejad <a.razinejad@gmail.com>
2021-01-19 17:21:16 +0330

    Rename the app

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('final-payment app is running!');
+    expect(await page.getTitleText()).toEqual('card-store app is running!');
   });
 
   afterEach(async () => {
