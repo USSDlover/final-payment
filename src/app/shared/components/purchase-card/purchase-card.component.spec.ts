a7cba211afa1a04f36c5e079d37af064a8f998a8
Alireza Razinejad <a.razinejad@gmail.com>
2021-01-21 11:17:55 +0330

    Implemented purchase card tests v0.8.54

--- src/app/shared/components/purchase-card/purchase-card.component.spec.ts
+++ src/app/shared/components/purchase-card/purchase-card.component.spec.ts
@@ -1,31 +1,50 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {HarnessLoader} from '@angular/cdk/testing';
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+
+import {MatCardHarness} from '@angular/material/card/testing';
 
 import {PurchaseCardComponent} from './purchase-card.component';
 import {CardNumberPipe} from '../../pipes';
 import {PurchaseModel} from '../../models';
 import {PurchaseInterface} from '../../interfaces';
+import {SharedModule} from '../../shared.module';
 
 describe('CreditCardComponent', () => {
-  let component: PurchaseCardComponent;
   let fixture: ComponentFixture<PurchaseCardComponent>;
-  let dummyPurchaseModel: PurchaseInterface;
+  let dummyPurchase: PurchaseInterface;
+  let component: PurchaseCardComponent;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
-    dummyPurchaseModel = PurchaseModel.dummy();
+    dummyPurchase = PurchaseModel.dummy();
     await TestBed.configureTestingModule({
+      imports: [SharedModule],
       declarations: [PurchaseCardComponent, CardNumberPipe]
-    })
-      .compileComponents();
+    }).compileComponents();
+    fixture = TestBed.createComponent(PurchaseCardComponent);
+    component = fixture.componentInstance;
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PurchaseCardComponent);
-    component = fixture.componentInstance;
-    component.purchase = dummyPurchaseModel;
+    component.purchase = dummyPurchase;
     fixture.detectChanges();
+    loader = TestbedHarnessEnvironment.loader(fixture);
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should have material card component', async () => {
+    const cards = await loader.getAllHarnesses(MatCardHarness);
+    expect(cards.length).toBe(1);
+  });
+
+  it('should have correct title', async () => {
+    const card = await loader.getHarness(MatCardHarness);
+    expect(await card.getTitleText()).toContain(dummyPurchase.holder);
+  });
+
+  it('should have correct subtitle', async () => {
+    const card = await loader.getHarness(MatCardHarness);
+    const cardNumber = await card.getSubtitleText();
+    expect(cardNumber.replace(/\s/g, '')).toContain(dummyPurchase.cardNumber);
   });
 });
