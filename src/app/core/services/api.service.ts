188bcb9cba14e9bd4c1fa0538f9214abee3e9196
Alireza Razinejad <a.razinejad@gmail.com>
2021-01-21 16:58:37 +0330

    Implement README.md v1.0.0

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -11,23 +11,22 @@ export class ApiService {
   constructor(private _http: HttpClient) {
   }
 
-  private static _handleError(err): Observable<any> {
+  private static _handleError<T>(err): Observable<T> {
     console.log('Error occur while tried to make http request', err);
-    return of({status: false, message: err.message});
+    return of({status: false, message: err.message} as unknown as T);
   }
 
   makeGetApiCall<T>(endPoint: string, queryParams?: HttpParams): Observable<T> {
     return this._http
       .get<T>(`${this.apiServer}/${endPoint}`, {params: queryParams})
-      .pipe(catchError((err => ApiService._handleError(err))));
+      .pipe(catchError((err => ApiService._handleError<T>(err))));
   }
 
-  makePostApiCall<T>(endPoint: string, body: any): Observable<T | boolean> {
-    // TODO: Remove the next line
-    return of(true);
+  makePostApiCall<T>(endPoint: string, body: any): Observable<T> {
+    // return of(true as unknown as T);
 
-    /*return this.http
+    return this._http
       .post<T>(`${this.apiServer}/${endPoint}`, body)
-      .pipe(catchError((err => this.handleError(err))));*/
+      .pipe(catchError(err => ApiService._handleError<T>(err)));
   }
 }
